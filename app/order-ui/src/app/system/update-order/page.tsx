bbad7cd3e9990e5a2660950c4fdc207d5f88100b
QuyetThangDev <quyetthang0501@gmail.com>
2025-05-20 13:51:05 +0700

    * ParentId: VOUCHER-01-FE (3) Task Id: VOUCHER-01-FE Type: Development Description: + Roll back to commit  83c3e492e34fe0d2f664a133756558b52b8af80a

--- app/order-ui/src/app/system/update-order/page.tsx
+++ app/order-ui/src/app/system/update-order/page.tsx
@@ -99,20 +99,7 @@ export default function UpdateOrderPage() {
     }, [])
 
     const handleClickPayment = () => {
-        // Update order type
-        let params: IUpdateOrderTypeRequest | null = null
-        if (type === OrderTypeEnum.AT_TABLE) {
-            params = { type: type, table: selectedTable?.slug || null, voucher: orderItems?.voucher?.slug || null }
-        } else {
-            params = { type: type, table: null, voucher: orderItems?.voucher?.slug || null }
-        }
-        updateOrderType({ slug: slug as string, params }, {
-            onSuccess: () => {
-                showToast(tToast('order.updateOrderSuccess'))
-                navigate(`${ROUTE.STAFF_ORDER_PAYMENT}?order=${orderItems?.slug}`)
-                refetch()
-            }
-        })
+        navigate(`${ROUTE.STAFF_ORDER_PAYMENT}?order=${orderItems?.slug}`)
     }
     // if (isPending) { return <UpdateOrderSkeleton /> }
 
