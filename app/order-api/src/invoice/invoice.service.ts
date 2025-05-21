f73e26255a1eba0c77a6afebeced11b549fdf4de
huu thiet <huuthietn01@gmail.com>
2025-05-21 14:00:38 +0700

    * ParentId: IV-01-BE (3) Task Id: IV-03-BE Type: Development Description: + Fix voucher value 2nd

--- app/order-api/src/invoice/invoice.service.ts
+++ app/order-api/src/invoice/invoice.service.ts
@@ -82,17 +82,17 @@ export class InvoiceService {
       throw new OrderException(OrderValidation.ORDER_NOT_FOUND);
     }
 
-    let voucherValue = 0;
+    const subtotalBeforeVoucher = order.orderItems?.reduce(
+      (total, current) => total + current.subtotal,
+      0,
+    );
+
+    let voucherValue = order.loss;
     if (order?.voucher?.type === VoucherType.PERCENT_ORDER) {
-      let voucherPercent = 100;
-      if (order.voucher.value > 0) {
-        voucherPercent = order.voucher.value;
-      }
-      const subtotalBeforeVoucher = (order.subtotal * 100) / voucherPercent;
-      voucherValue = subtotalBeforeVoucher - order.subtotal + order.loss;
+      voucherValue += (subtotalBeforeVoucher * order.voucher.value) / 100;
     }
     if (order?.voucher?.type === VoucherType.FIXED_VALUE) {
-      voucherValue = order.voucher.value + order.loss;
+      voucherValue += order.voucher.value;
     }
 
     // invoice exists
@@ -102,9 +102,7 @@ export class InvoiceService {
         context,
       );
       Object.assign(order.invoice, {
-        subtotalBeforeVoucher: order.invoice.amount + voucherValue,
-        // (order.invoice.amount * 100) /
-        // (order.invoice.voucherValue !== 0 ? order.invoice.voucherValue : 100),
+        subtotalBeforeVoucher,
       });
       return order.invoice;
     }
@@ -161,9 +159,7 @@ export class InvoiceService {
     );
 
     Object.assign(invoice, {
-      subtotalBeforeVoucher: invoice.amount + voucherValue,
-      // (invoice.amount * 100) /
-      // (invoice.voucherValue !== 0 ? invoice.voucherValue : 100),
+      subtotalBeforeVoucher,
     });
 
     return invoice;
