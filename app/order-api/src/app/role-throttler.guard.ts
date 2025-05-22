f1d0d03d30789d09099c15472a32f69f948990aa
huu thiet <huuthietn01@gmail.com>
2025-05-22 14:43:25 +0700

    * ParentId: RL-01-BE (3) Task Id: RL-02-BE Type: Development Description: + Get global limit, ttl in app.module when import

--- app/order-api/src/app/role-throttler.guard.ts
+++ app/order-api/src/app/role-throttler.guard.ts
@@ -7,7 +7,13 @@ import {
 } from '@nestjs/throttler';
 import { roleThrottlerConfig } from 'src/config/role-throttler.config';
 import { Reflector } from '@nestjs/core';
+import * as _ from 'lodash';
 
+// Apply cases:
+// Api private: check role
+// Api public(common use for both login and no login): use @SkipThrottle()
+// Api public(use only for no login user - api has the tail is 'public'): role is 'GUEST'
+// get from roleThrottlerConfig
 @Injectable()
 export class RoleThrottlerGuard extends ThrottlerGuard {
   constructor(
@@ -18,10 +24,28 @@ export class RoleThrottlerGuard extends ThrottlerGuard {
     super(options, storageService, reflector);
   }
 
+  // if use @SkipThrottle() => handleRequest will be skipped
   protected async handleRequest(
     requestProps: ThrottlerRequest,
   ): Promise<boolean> {
     const { context } = requestProps;
+
+    // set up in app.module.ts
+    // ThrottlerModule.forRoot([
+    //   {
+    //     ttl: 60000,
+    //     limit: 1000,
+    //   },
+    // ]),
+    let globalOptions;
+    if (Array.isArray(this.options)) {
+      globalOptions = _.first(this.options);
+    } else if (
+      typeof this.options === 'object' &&
+      Array.isArray(this.options.throttlers)
+    ) {
+      globalOptions = _.first(this.options.throttlers);
+    }
 
     const { req } = this.getRequestResponse(context);
     const role = req?.user?.scope?.role || 'GUEST';
@@ -40,8 +64,9 @@ export class RoleThrottlerGuard extends ThrottlerGuard {
     );
 
     const fallbackConfig = roleThrottlerConfig[role];
-    const limit = limitFromDecorator ?? fallbackConfig.limit;
-    const ttl = ttlFromDecorator ?? fallbackConfig.ttl;
+    const limit =
+      limitFromDecorator ?? fallbackConfig.limit ?? globalOptions.limit;
+    const ttl = ttlFromDecorator ?? fallbackConfig.ttl ?? globalOptions.ttl;
 
     const tracker = await this.getTracker(req);
     const handler = context.getHandler();
