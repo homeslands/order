bb544401e72fc7fcda78bf42de13b38ec48de029
QuyetThangDev <quyetthang0501@gmail.com>
2025-05-22 15:03:36 +0700

    * ParentId: VOUCHER-01-FE (5) Task Id: VOUCHER-01-FE Type: Development Description: + Fix voucher end date error when display + Update voucher page and export voucher

--- app/order-ui/src/components/app/dialog/index.tsx
+++ app/order-ui/src/components/app/dialog/index.tsx
@@ -68,6 +68,7 @@ export { default as QrCodeDialog } from './qr-code-dialog'
 export { default as CreateVoucherDialog } from './confirm-create-voucher-dialog'
 export { default as ConfirmUpdateVoucherDialog } from './confirm-update-voucher-dialog'
 export { default as DeleteVoucherDialog } from './delete-voucher-dialog'
+export { default as ConfirmExportVoucherDialog } from './confirm-export-voucher-dialog'
 
 export { default as ConfirmCreateVoucherGroupDialog } from './confirm-create-voucher-group-dialog'
 export { default as ConfirmUpdateVoucherGroupDialog } from './confirm-update-voucher-group-dialog'
