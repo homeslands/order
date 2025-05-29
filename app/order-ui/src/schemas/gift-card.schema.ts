762f47bd635fd0e405d3ccccdbbaab12a19adc82
tuan.do <doduongthaituan201102@gmail.com>
2025-05-30 01:57:55 +0700

    * ParentId: GC-42-FE (1) Task Id: GC-42 Type: Development Description: + Fix bug 24, 25 + Add input seperator + Remove action dropdown, add button tooltip + Fix layout error

--- app/order-ui/src/schemas/gift-card.schema.ts
+++ app/order-ui/src/schemas/gift-card.schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 export const createGiftCardSchema = z.object({
-  title: z.string().min(1),
-  description: z.optional(z.string()),
+  title: z.string().min(1).max(500),
+  description: z.optional(z.string().max(1000)),
   file: z.optional(z.instanceof(File)),
   points: z.coerce.number().min(1000).max(10000000),
   price: z.coerce.number().min(1000).max(10000000),
@@ -11,8 +11,8 @@ export const createGiftCardSchema = z.object({
 
 export const updateGiftCardSchema = z.object({
   slug: z.string().min(1),
-  title: z.string().min(1),
-  description: z.optional(z.string()),
+  title: z.string().min(1).max(500),
+  description: z.optional(z.string().max(1000)),
   points: z.coerce.number().min(1000).max(10000000),
   price: z.coerce.number().min(1000).max(10000000),
   file: z.optional(z.instanceof(File)),
