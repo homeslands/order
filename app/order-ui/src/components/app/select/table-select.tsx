da18745573e6dbe65c9311b8f8ac8fffb874bca4
QuyetThangDev <quyetthang0501@gmail.com>
2025-05-30 10:16:35 +0700

    * ParentId: OR-524-FE (4) Task Id: OR-524-FE Type: Development Description: + Use branch from branch store in client cart page

--- app/order-ui/src/components/app/select/table-select.tsx
+++ app/order-ui/src/components/app/select/table-select.tsx
@@ -40,13 +40,6 @@ export default function TableSelect({ tableOrder, onTableSelect }: ITableSelectP
             setSelectedTableId(tableOrder.slug)
         }
     }, [tableOrder])
-    // const tableList = [...(tables?.result || [])].sort((a, b) => {
-    //     if (a.status !== b.status) {
-    //         // Đảo ngược thứ tự: RESERVED < AVAILABLE
-    //         return a.status === TableStatus.RESERVED ? -1 : 1
-    //     }
-    //     return Number(a.name) - Number(b.name)
-    // })
 
     if (cartItems?.type === OrderTypeEnum.TAKE_OUT) {
         return null
