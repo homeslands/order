089d0ffdd035d8b41af47956d884a2fb58044363
tuan.do <doduongthaituan201102@gmail.com>
2025-06-04 02:52:23 +0700

    * ParentId: GC-08-BE (2) Task Id: GC-36 Type: Development Description: + Create/GetAll/GetOne card order

--- app/order-api/src/gift-card-modules/receipient/dto/create-receipient.dto.ts
+++ app/order-api/src/gift-card-modules/receipient/dto/create-receipient.dto.ts
@@ -1,1 +1,23 @@
-export class CreateReceipientDto {}
+import { IsOptional } from "class-validator";
+
+import { IsNumber } from "class-validator";
+
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { IsNotEmpty } from "class-validator";
+
+export class CreateReceipientDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    recipientSlug: string;
+  
+    @IsNumber()
+    @IsNotEmpty()
+    @ApiProperty()
+    quantity: number;
+  
+    @IsOptional()
+    @ApiProperty()
+    message?: string;
+  }
