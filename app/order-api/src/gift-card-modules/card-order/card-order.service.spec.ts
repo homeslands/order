16aa69f4c85b6cc561269dd379e9e7a256dfb3f6
tuan.do <doduongthaituan201102@gmail.com>
2025-06-05 13:35:26 +0700

    * ParentId: GC-08-BE (5) Task Id: GC-36 Type: Development Description: + Fixed: unit test error

--- app/order-api/src/gift-card-modules/card-order/card-order.service.spec.ts
+++ app/order-api/src/gift-card-modules/card-order/card-order.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardOrderService } from './card-order.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { repositoryMockFactory } from 'src/test-utils/repository-mock.factory';
+import { Card } from '../card/entities/card.entity';
+import { CardOrder } from './entities/card-order.entity';
+import { MAPPER_MODULE_PROVIDER } from 'src/app/app.constants';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { mapperMockFactory } from 'src/test-utils/mapper-mock.factory';
+import { dataSourceMockFactory } from 'src/test-utils/datasource-mock.factory';
+import { DataSource } from 'typeorm';
+import { TransactionManagerService } from 'src/db/transaction-manager.service';
 
 describe('CardOrderService', () => {
   let service: CardOrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CardOrderService],
+      providers: [
+        CardOrderService,
+        TransactionManagerService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(Card),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(CardOrder),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: MAPPER_MODULE_PROVIDER,
+          useFactory: mapperMockFactory,
+        },
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console,
+        },
+        {
+          provide: DataSource,
+          useFactory: dataSourceMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<CardOrderService>(CardOrderService);
--- app/order-api/src/gift-card-modules/card-order/card-order.validation.ts
+++ app/order-api/src/gift-card-modules/card-order/card-order.validation.ts
@@ -4,12 +4,22 @@ export const ERROR_WHEN_CREATE_CARD_ORDER = 'ERROR_WHEN_CREATE_CARD_ORDER';
 export const CARD_ORDER_NOT_FOUND = 'CARD_ORDER_NOT_FOUND';
 export const ERROR_WHEN_UPDATE_CARD_ORDER = 'ERROR_WHEN_UPDATE_CARD_ORDER';
 export const ERROR_WHEN_REMOVE_CARD_ORDER = 'ERROR_WHEN_REMOVE_CARD_ORDER';
+export const CARD_ORDER_CUSTOMER_NOT_FOUND = 'CARD_ORDER_CUSTOMER_NOT_FOUND';
+export const CARD_ORDER_TOTAL_AMOUNT_NOT_CORRECT =
+  'CARD_ORDER_TOTAL_AMOUNT_NOT_CORRECT';
+export const CARD_ORDER_TOTAL_QUANTITY_NOT_CORRECT =
+  'CARD_ORDER_TOTAL_QUANTITY_NOT_CORRECT';
+export const CARD_ORDER_RECIPIENT_NOT_FOUND = 'CARD_ORDER_RECIPIENT_NOT_FOUND';
 
 export type TCardOrderErrorCodeKey =
   | typeof ERROR_WHEN_CREATE_CARD_ORDER
   | typeof CARD_ORDER_NOT_FOUND
   | typeof ERROR_WHEN_UPDATE_CARD_ORDER
-  | typeof ERROR_WHEN_REMOVE_CARD_ORDER;
+  | typeof ERROR_WHEN_REMOVE_CARD_ORDER
+  | typeof CARD_ORDER_CUSTOMER_NOT_FOUND
+  | typeof CARD_ORDER_TOTAL_AMOUNT_NOT_CORRECT
+  | typeof CARD_ORDER_TOTAL_QUANTITY_NOT_CORRECT
+  | typeof CARD_ORDER_RECIPIENT_NOT_FOUND;
 
 // 158101- 158200
 export type TCardOrderErrorCode = Record<
@@ -30,5 +40,18 @@ export const CardOrderValidation: TCardOrderErrorCode = {
   ERROR_WHEN_REMOVE_CARD_ORDER: createErrorCode(
     158104,
     'Error when remove card order',
+  ),
+  CARD_ORDER_CUSTOMER_NOT_FOUND: createErrorCode(158105, 'Customer not found'),
+  CARD_ORDER_TOTAL_AMOUNT_NOT_CORRECT: createErrorCode(
+    158106,
+    'Total amount not correct',
+  ),
+  CARD_ORDER_TOTAL_QUANTITY_NOT_CORRECT: createErrorCode(
+    158107,
+    'Total quantity not correct',
+  ),
+  CARD_ORDER_RECIPIENT_NOT_FOUND: createErrorCode(
+    158108,
+    'Recipient not found',
   ),
 };
