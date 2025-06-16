2b48d74d7d98e81d902f7a0c108d3baa0f7ae570
MyTC <mytranchi2508@gmail.com>
2025-06-16 08:11:52 +0700

    * ParentId: GC-09-FE (3)
    Task Id: GC-31,
    Type: Fix Bug
    Description:
    - GC-77

--- app/order-ui/src/app/client/gift-card/components/gift-card-sheet.tsx
+++ app/order-ui/src/app/client/gift-card/components/gift-card-sheet.tsx
@@ -291,6 +291,7 @@ export default function GiftCardSheet() {
                     }}
                   />
                 )}
+              </div>
               </div>{' '}
               {/* Fixed Footer - Total Section */}{' '}
               {giftCardItem && (
