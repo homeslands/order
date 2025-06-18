b9162a72a7c40fa10864fa574fba27ca842bf30b
tuan.do <doduongthaituan201102@gmail.com>
2025-06-18 09:58:26 +0700

    * ParentId: GC-82-FE (1) Task Id: GC-73 Type: Bug Description: + Fixed error message + Fixed Dark mode error

--- app/order-ui/src/schemas/gift-card.schema.ts
+++ app/order-ui/src/schemas/gift-card.schema.ts
@@ -1,23 +1,57 @@
 import { z } from 'zod'
+import { TFunction } from 'i18next';
 
-export const createGiftCardSchema = z.object({
-  title: z.string().min(1).max(500),
-  description: z.optional(z.string().max(1000)),
-  file: z.optional(z.instanceof(File)),
-  points: z.coerce.number().min(1000).max(10000000),
-  price: z.coerce.number().min(1000).max(10000000),
-  isActive: z.boolean().default(true),
-})
+const titleSchema = (t: TFunction) => z
+  .string({ message: t("schema.title.required") })
+  .trim()
+  .min(1, t("schema.title.min"))
+  .max(500, t('schema.title.max'));
 
-export const updateGiftCardSchema = z.object({
-  slug: z.string().min(1),
-  title: z.string().min(1).max(500),
-  description: z.optional(z.string().max(1000)),
-  points: z.coerce.number().min(1000).max(10000000),
-  price: z.coerce.number().min(1000).max(10000000),
-  file: z.optional(z.instanceof(File)),
-  isActive: z.boolean().default(true),
-})
+const descriptionSchema = (t: TFunction) => z
+  .string()
+  .trim()
+  .max(1000, t('schema.description.max'))
+  .optional();
 
-export type TCreateGiftCardSchema = z.infer<typeof createGiftCardSchema>
-export type TUpdateGiftCardSchema = z.infer<typeof updateGiftCardSchema>
+const fileSchema = z
+  .instanceof(File)
+  .optional();
+
+const pointsSchema = (t: TFunction) => z.coerce
+  .number({
+    required_error: t('schema.points.required'),
+    invalid_type_error: t('schema.points.invalid'),
+  })
+  .min(1000, t('schema.points.min'))
+  .max(10_000_000, t('schema.points.max'));
+
+const priceSchema = (t: TFunction) => z.coerce
+  .number({
+    required_error: t('schema.price.required'),
+    invalid_type_error: t('schema.price.invalid'),
+  })
+  .min(1000, t('schema.price.min'))
+  .max(10_000_000, t('schema.price.max'));
+
+const isActiveSchema = z.boolean().default(true);
+
+// Create Schema
+export const createGiftCardSchema = (t: TFunction) => z.object({
+  title: titleSchema(t),
+  description: descriptionSchema(t),
+  file: fileSchema,
+  points: pointsSchema(t),
+  price: priceSchema(t),
+  isActive: isActiveSchema,
+});
+
+// Update Schema
+export const updateGiftCardSchema = (t: TFunction) => createGiftCardSchema(t).extend({
+  slug: z
+    .string()
+    .min(1),
+});
+
+
+export type TCreateGiftCardSchema = z.infer<ReturnType<typeof createGiftCardSchema>>
+export type TUpdateGiftCardSchema = z.infer<ReturnType<typeof updateGiftCardSchema>>
