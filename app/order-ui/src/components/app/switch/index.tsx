9baf64b9fe2ba12983cffc83c06975ce88c43dbf
QuyetThangDev <quyetthang0501@gmail.com>
2025-06-23 21:57:25 +0700

    TaskId: TRE-33 add isCombo field to product create and update forms (1)
    Type: Development
    
    Description:
    - Added `isCombo` checkbox to product creation and editing forms

--- app/order-ui/src/components/app/switch/index.tsx
+++ app/order-ui/src/components/app/switch/index.tsx
@@ -4,4 +4,5 @@ export { default as IsLimitSwitch } from './is-limit-switch'
 export { default as IsTemplateSwitch } from './is-template-switch'
 export { default as IsTopSaleSwitch } from './is-top-sell-switch'
 export { default as IsNewProductSwitch } from './is-new-switch'
+export { default as IsComboSwitch } from './is-combo-switch'
 
