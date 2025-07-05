2e4ad43a99ce9ea4cfdb6d957416407d3efd9428
tuan.do <doduongthaituan201102@gmail.com>
2025-07-06 00:43:44 +0700

    * ParentId: GC-12-BE (1) Task Id: GC-118 Type: Development Description: + Implement use gift card API

--- app/order-api/src/gift-card-modules/gift-card/gift-card.controller.ts
+++ app/order-api/src/gift-card-modules/gift-card/gift-card.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ApiResponseWithType } from 'src/app/app.decorator';
 import { GiftCardResponseDto } from './dto/gift-card-response.dto';
 import { GiftCardService } from './gift-card.service';
 import { AppResponseDto } from 'src/app/app.dto';
+import { UseGiftCardDto } from './dto/use-gift-card.dto';
 
 @Controller('gift-card')
 @ApiTags('Gift Card Resource')
@@ -40,6 +50,25 @@ export class GiftCardController {
   })
   async findOne(@Param('slug') slug: string) {
     const result = await this.gcService.findOne(slug);
+    return {
+      statusCode: HttpStatus.OK,
+      timestamp: new Date().toISOString(),
+      result,
+    } as AppResponseDto<GiftCardResponseDto>;
+  }
+
+  @Post('/use')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Use the gift card' })
+  @ApiResponseWithType({
+    status: HttpStatus.OK,
+    type: GiftCardResponseDto,
+  })
+  async use(
+    @Body(new ValidationPipe({ transform: true, whitelist: true }))
+    req: UseGiftCardDto,
+  ) {
+    const result = await this.gcService.use(req);
     return {
       statusCode: HttpStatus.OK,
       timestamp: new Date().toISOString(),
