47c998e01c372590fe9d0817770acc782fa9b2de
canhnlse172651 <canhnlse172651@gmail.com>
2025-07-12 11:25:06 +0700

    TaskId: TRE-59-BE (1) validate register payload
    Type: Fixbug
    
    Description:
    - handle regex Vietnamese number phone and email format

--- app/order-api/src/auth/constants.ts
+++ app/order-api/src/auth/constants.ts
@@ -3,6 +3,7 @@ export const jwtConstants = {
     'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
 };
 
+<<<<<<< HEAD
 export enum AccountVerificationType {
   MAIL = 'mail',
   PHONE_NUMBER = 'phone-number',
@@ -12,3 +13,7 @@ export enum AccountVerificationStatus {
   VERIFIED = 'verified',
   UNVERIFIED = 'unverified',
 }
+=======
+// Vietnamese phone number regex pattern
+export const VIETNAMESE_PHONE_REGEX = /^(0[3|5|7|8|9][0-9]{8})$/;
+>>>>>>> d9534006 (TaskId: TRE-59-BE (1) validate register payload)
