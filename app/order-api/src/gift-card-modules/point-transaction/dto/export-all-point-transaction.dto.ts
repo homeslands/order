c05a7764ee721dd0efa6df71a1de204315bcac80
tuan.do <doduongthaituan201102@gmail.com>
2025-07-29 15:42:49 +0700

    * Parent ID: GC-149-BE (1) Task Id: GC-166 Type: Development Description: + Implement API

--- app/order-api/src/gift-card-modules/point-transaction/dto/export-all-point-transaction.dto.ts
+++ app/order-api/src/gift-card-modules/point-transaction/dto/export-all-point-transaction.dto.ts
@@ -1,9 +1,31 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import moment from 'moment';
 
 export class ExportAllPointTransactionDto {
   @IsNotEmpty()
   @IsString()
   @ApiProperty()
   userSlug: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsOptional()
+  @Transform(({ value }) => moment(value, 'YYYY-MM-DD').startOf('day').toDate())
+  fromDate: Date;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsOptional()
+  @Transform(({ value }) => moment(value, 'YYYY-MM-DD').endOf('day').toDate())
+  toDate: Date;
+
+  @ApiProperty({
+    required: false,
+  })
+  @IsOptional()
+  type: string;
 }
