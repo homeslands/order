57e35cfc5d28a7c9a068c26129c918335b3caca7
nguyen083 <billbatri085@gmail.com>
2025-08-07 10:11:08 +0700

    TaskId: TRE-124-FE (1) Update order management routes and components
    Type: Bugs
    
    Description:
    - Rename all routes related to order management to delivery management
    - Add new components for delivery management, including customer information, order list, and related actions
    - Update language files to reflect these changes

--- app/order-ui/src/app/system/order-management/index.tsx
+++ app/order-ui/src/app/system/order-management/index.tsx
@@ -1,8 +1,4 @@
 export { default as CustomerInformation } from './customer-information'
-export { default as OrderItemDetail } from './order-item-detail'
-export { default as OrderItemList } from './order-item-list'
+export { default as OrderDetailPage } from './order-detail-page'
+export { default as OrderHistoryItem } from './order-history-item'
 export { default as OrderManagementPage } from './page'
-export { default as OrderWaitListCounting } from './order-wait-list-counting'
-export { default as TotalOrders } from './total-orders'
-export { default as OrderInformation } from './order-general-information'
-
