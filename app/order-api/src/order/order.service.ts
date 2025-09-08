6461473d5ddd37dccfcc40a6501c043b41ad2a8c
huu thiet <huuthietn01@gmail.com>
2025-09-08 15:01:02 +0700

    * ParentId: TRE-210-BE (4) Task Id: TRE-210-BE Type: Development Description: - Delete reservation point when update, create, delete order item, update order voucher

--- app/order-api/src/order/order.service.ts
+++ app/order-api/src/order/order.service.ts
@@ -157,7 +157,7 @@ export class OrderService {
     const removedOrder = await this.transactionManagerService.execute<Order>(
       async (manager) => {
         // Cancel accumulated points reservation
-        await this.accumulatedPointService.handleOrderCancellation(order.id);
+        await this.accumulatedPointService.handleCancelReservation(order.id);
         // Update subtotal and accumulated points to use in order
         const subtotalBeforeUseAccumulatedPoints =
           order.subtotal + order.accumulatedPointsToUse;
@@ -443,6 +443,11 @@ export class OrderService {
           previousVoucher.remainingUsage += 1;
           await manager.save(previousVoucher);
         }
+
+        // Cancel accumulated points reservation
+        await this.accumulatedPointService.handleCancelReservation(order.id);
+        // Update accumulated points to use in order
+        order.accumulatedPointsToUse = 0;
 
         return await manager.save(order);
       },
