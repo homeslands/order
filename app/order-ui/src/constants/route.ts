53397fe504f61ffa2c0a4333d37e4a0a5de030fc
QuyetThangDev <quyetthang0501@gmail.com>
2025-09-09 14:32:14 +0700

    TaskId: TRE-217-FE (1) Implement loyalty point
    Type: Development
    
    Description:
    - Add loyalty point selector when paying

--- app/order-ui/src/constants/route.ts
+++ app/order-ui/src/constants/route.ts
@@ -49,6 +49,7 @@ export const ROUTE = {
   STAFF_GIFT_CARD_FEATURE_FLAG: '/system/feature-lock-management',
   STAFF_GIFT_CARD_CHECKOUT: '/system/card/catalog/checkout',
   STAFF_GIFT_CARD_CHECKOUT_WITH_SLUG: '/system/card/catalog/checkout/:slug',
+  STAFF_LOYALTY_POINT: '/system/loyalty-point',
 
   //Client Routes
   CLIENT_HOME: '/',
