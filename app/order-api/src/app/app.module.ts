6eea1ddc74e7329a254b14f697a15044250841d7
huu thiet <huuthietn01@gmail.com>
2025-09-11 14:43:46 +0700

    * ParentId: TRE-223-BE (1) Task Id: TRE-223-BE Type: Development Description: - Implement new jwt guard

--- app/order-api/src/app/app.module.ts
+++ app/order-api/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { validate } from './env.validation';
 import { AuthModule } from 'src/auth/auth.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { FileModule } from 'src/file/file.module';
 import { HealthModule } from 'src/health/health.module';
@@ -38,7 +38,7 @@ import { WorkflowModule } from 'src/workflow/workflow.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { resolve } from 'path';
 import { DbModule } from 'src/db/db.module';
-import { JwtAuthGuard } from 'src/auth/passport/jwt/jwt-auth.guard';
+// import { JwtAuthGuard } from 'src/auth/passport/jwt/jwt-auth.guard';
 import { RoleModule } from 'src/role/role.module';
 import { RolesGuard } from 'src/role/roles.guard';
 import { SystemConfigModule } from 'src/system-config/system-config.module';
@@ -70,6 +70,8 @@ import { PrinterModule } from 'src/printer/printer.module';
 import { ZaloOaConnectorModule } from 'src/zalo-oa-connector/zalo-oa-connector.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { InvoiceAreaModule } from 'src/invoice-area/invoice-area.module';
+import { RoleBasedSerializationInterceptor } from 'src/role/role.interceptor';
+import { JwtOptionalAuthGuard } from 'src/auth/passport/jwt/jwt-optional-auth.guard';
 
 @Module({
   imports: [
@@ -171,9 +173,13 @@ import { InvoiceAreaModule } from 'src/invoice-area/invoice-area.module';
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
     },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard,
+    // },
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard,
+      useClass: JwtOptionalAuthGuard,
     },
     {
       provide: APP_GUARD,
@@ -182,6 +188,10 @@ import { InvoiceAreaModule } from 'src/invoice-area/invoice-area.module';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RoleBasedSerializationInterceptor,
     },
   ],
 })
