5bb1d5d201038d8948bf046c8bd2fdfcaee3d51d
QuyetThangDev <quyetthang0501@gmail.com>
2025-09-21 21:52:54 +0700

    TaskId: TRE-238-FE (1) Add full name field in user registration form
    Type: Development
    
    Description:
    - Add full name field in user registration form

--- app/order-ui/src/components/app/form/register-form.tsx
+++ app/order-ui/src/components/app/form/register-form.tsx
@@ -38,6 +38,9 @@ export const RegisterForm: React.FC<IFormRegisterProps> = ({
   const form = useForm<TRegisterSchema>({
     resolver: zodResolver(useRegisterSchema()),
     defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
       phonenumber: '',
       password: '',
       confirmPassword: '',
@@ -49,21 +52,7 @@ export const RegisterForm: React.FC<IFormRegisterProps> = ({
   }
 
   const formFields = {
-    // email: (
-    //   <FormField
-    //     control={form.control}
-    //     name="email"
-    //     render={({ field }) => (
-    //       <FormItem>
-    //         <FormLabel>{t('login.email')}</FormLabel>
-    //         <FormControl>
-    //           <Input placeholder={t('login.enterEmail')} {...field} />
-    //         </FormControl>
-    //         <FormMessage />
-    //       </FormItem>
-    //     )}
-    //   />
-    // ),
+
     phonenumber: (
       <FormField
         control={form.control}
@@ -132,43 +121,58 @@ export const RegisterForm: React.FC<IFormRegisterProps> = ({
         )}
       />
     ),
-    // firstName: (
-    //   <FormField
-    //     control={form.control}
-    //     name="firstName"
-    //     render={({ field }) => (
-    //       <FormItem>
-    //         <FormLabel>{t('login.firstName')}</FormLabel>
-    //         <FormControl>
-    //           <Input placeholder={t('login.enterFirstName')} {...field} />
-    //         </FormControl>
-    //         <FormMessage />
-    //       </FormItem>
-    //     )}
-    //   />
-    // ),
-    // lastName: (
-    //   <FormField
-    //     control={form.control}
-    //     name="lastName"
-    //     render={({ field }) => (
-    //       <FormItem>
-    //         <FormLabel>{t('login.lastName')}</FormLabel>
-    //         <FormControl>
-    //           <Input placeholder={t('login.enterLastName')} {...field} />
-    //         </FormControl>
-    //         <FormMessage />
-    //       </FormItem>
-    //     )}
-    //   />
-    // ),
+    firstName: (
+      <FormField
+        control={form.control}
+        name="firstName"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('login.firstName')}</FormLabel>
+            <FormControl>
+              <Input placeholder={t('login.enterFirstName')} {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    ),
+    lastName: (
+      <FormField
+        control={form.control}
+        name="lastName"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('login.lastName')}</FormLabel>
+            <FormControl>
+              <Input placeholder={t('login.enterLastName')} {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    ),
+    email: (
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{t('login.email')}</FormLabel>
+            <FormControl>
+              <Input placeholder={t('login.enterEmail')} {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    ),
   }
 
   return (
     <div className="mt-3">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
-          <div className="grid grid-cols-1 gap-2 text-white">
+          <div className="grid grid-cols-1 gap-2 text-white sm:grid-cols-2">
             {Object.keys(formFields).map((key) => (
               <React.Fragment key={key}>
                 {formFields[key as keyof typeof formFields]}
@@ -201,7 +205,7 @@ export const RegisterForm: React.FC<IFormRegisterProps> = ({
           </div>
           <Button
             type="submit"
-            className="w-full mt-5"
+            className="mt-5 w-full"
             disabled={isLoading || !isTermsAccepted}
           >
             {isLoading ? <ButtonLoading /> : t('register.title')}
