b19029e8404dae02af552e551bcd1c46c9cec42b
QuyetThangDev <quyetthang0501@gmail.com>
2025-09-23 09:07:59 +0700

    TaskId: TRE-237-FE (2) Implement customer order management page for staff and admin
    Type: Development
    
    Description:
    - Merge accumulated point into customer detail page

--- app/order-ui/src/app/system/customers/DataTable/columns/customer-columns.tsx
+++ app/order-ui/src/app/system/customers/DataTable/columns/customer-columns.tsx
@@ -1,24 +1,14 @@
+import moment from 'moment'
 import { ColumnDef } from '@tanstack/react-table'
 import { useTranslation } from 'react-i18next'
-import moment from 'moment'
-import { MoreHorizontal, ShoppingBag } from 'lucide-react'
 
 import {
-  Button,
   DataTableColumnHeader,
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
 } from '@/components/ui'
 import { IUserInfo } from '@/types'
-import { ResetPasswordDialog, UpdateCustomerDialog, UserInfoDialog } from '@/components/app/dialog'
-import { NavLink } from 'react-router-dom'
-import { ROUTE } from '@/constants'
 
 export const useUserListColumns = (): ColumnDef<IUserInfo>[] => {
-  const { t } = useTranslation(['customer', 'common'])
-  const { t: tCommon } = useTranslation(['common'])
+  const { t } = useTranslation(['customer'])
   return [
     {
       accessorKey: 'createdAt',
@@ -76,39 +66,6 @@ export const useUserListColumns = (): ColumnDef<IUserInfo>[] => {
       cell: ({ row }) => {
         const user = row.original
         return <div className="text-sm">{user?.email}</div>
-      },
-    },
-    {
-      id: 'actions',
-      header: tCommon('common.action'),
-      cell: ({ row }) => {
-        const user = row.original
-        return (
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="p-0 w-8 h-8">
-                  <span className="sr-only">{tCommon('common.action')}</span>
-                  <MoreHorizontal className="w-4 h-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>
-                  {tCommon('common.action')}
-                </DropdownMenuLabel>
-                <NavLink to={`${ROUTE.STAFF_CUSTOMER_MANAGEMENT}/${user.slug}/order-history`}>
-                  <Button variant="ghost" className="flex gap-1 justify-start px-2 w-full text-sm">
-                    <ShoppingBag className="icon" />
-                    {t('customer.viewOrders')}
-                  </Button>
-                </NavLink>
-                <UserInfoDialog user={user} />
-                <ResetPasswordDialog user={user} />
-                <UpdateCustomerDialog customer={user} />
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        )
       },
     },
   ]
