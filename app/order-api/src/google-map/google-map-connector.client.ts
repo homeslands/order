9c5db460076c35faba6b0af2418302de729d070a
huu thiet <huuthietn01@gmail.com>
2025-09-23 09:38:24 +0700

    * ParentId: TRE-233-BE (3) Task Id: TRE-233-BE Type: Development Description: - Refactor api get address suggestion

--- app/order-api/src/google-map/google-map-connector.client.ts
+++ app/order-api/src/google-map/google-map-connector.client.ts
@@ -9,18 +9,23 @@ import {
   DirectionsResponseDto,
   DistanceAndDurationResponseDto,
   DistanceMatrixResponseDto,
+  GeocodingAddressResponseDto,
+  GeocodingAddressResultDto,
   LocationResponseDto,
   PlaceAutocompleteResponseDto,
   PlaceDetailsResponseDto,
   PlaceDetailsResultResponseDto,
   RouteAndDirectionResponseDto,
+  SuggestionAddressResponseDto,
+  SuggestionAddressResultResponseDto,
 } from './dto/google-map.response.dto';
 import { GoogleMapException } from './google-map.exception';
 import { GoogleMapValidation } from './google-map.validation';
+import { SystemConfigService } from 'src/system-config/system-config.service';
+import { SystemConfigKey } from 'src/system-config/system-config.constant';
 
 @Injectable()
 export class GoogleMapConnectorClient {
-  private googleMapsApiUrl: string;
   private googleMapsApiKey: string;
 
   constructor(
@@ -28,17 +33,28 @@ export class GoogleMapConnectorClient {
     @Inject(WINSTON_MODULE_NEST_PROVIDER)
     private readonly logger: Logger,
     private readonly configService: ConfigService,
+    private readonly systemConfigService: SystemConfigService,
   ) {
-    this.googleMapsApiUrl =
-      this.configService.get<string>('GOOGLE_MAP_API_URL');
     this.googleMapsApiKey = this.configService.get<string>(
       'GOOGLE_MAPS_API_KEY',
     );
   }
 
-  async getAddress(name: string): Promise<AutocompleteAddressResponseDto[]> {
-    const context = `${GoogleMapConnectorClient.name}.${this.getAddress.name}`;
-    const requestUrl = `${await this.googleMapsApiUrl}/place/autocomplete/json?input=${encodeURIComponent(
+  async getGoogleMapsApiUrl() {
+    return this.systemConfigService.get(SystemConfigKey.GOOGLE_MAPS_API_URL);
+  }
+
+  async getPlacesGoogleMapsApiUrlV1() {
+    return this.systemConfigService.get(
+      SystemConfigKey.PLACES_GOOGLE_MAPS_API_URL_V1,
+    );
+  }
+
+  async getAutocompleteAddress(
+    name: string,
+  ): Promise<AutocompleteAddressResponseDto[]> {
+    const context = `${GoogleMapConnectorClient.name}.${this.getAutocompleteAddress.name}`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/place/autocomplete/json?input=${encodeURIComponent(
       name,
     )}&types=geocode&language=vi&key=${this.googleMapsApiKey}`;
 
@@ -82,9 +98,94 @@ export class GoogleMapConnectorClient {
     );
   }
 
+  async getGeocodingAddress(
+    name: string,
+  ): Promise<GeocodingAddressResultDto[]> {
+    const context = `${GoogleMapConnectorClient.name}.${this.getGeocodingAddress.name}`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/geocode/json?address=${encodeURIComponent(
+      name,
+    )}&components=country:VN&key=${this.googleMapsApiKey}`;
+
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<GeocodingAddressResponseDto>(requestUrl, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .pipe(
+          catchError((error: AxiosError) => {
+            this.logger.error(
+              `Get address failed: ${JSON.stringify(error.response?.data)}`,
+              error.stack,
+              context,
+            );
+            throw new GoogleMapException(
+              GoogleMapValidation.ERROR_WHEN_GET_ADDRESS,
+              error.message,
+            );
+          }),
+        ),
+    );
+
+    if (data.status !== 'OK') {
+      this.logger.error(`Get geocoding address failed: ${name}`, context);
+      throw new GoogleMapException(
+        GoogleMapValidation.ERROR_WHEN_GET_ADDRESS,
+        data.status,
+      );
+    }
+
+    return data.results;
+  }
+
+  async getSuggestionAddress(
+    name: string,
+  ): Promise<SuggestionAddressResultResponseDto[]> {
+    const context = `${GoogleMapConnectorClient.name}.${this.getGeocodingAddress.name}`;
+    const requestUrl = `${await this.getPlacesGoogleMapsApiUrlV1()}/places:autocomplete?key=${this.googleMapsApiKey}`;
+
+    const body = {
+      input: name,
+      includedRegionCodes: ['VN'],
+      languageCode: 'vi',
+      // includeQueryPredictions: false,
+      // locationBias: {
+      //   circle: {
+      //     center: { latitude: 10.7769, longitude: 106.7009 }, // ví dụ HCM
+      //     radius: 50000, // mét
+      //   },
+      // },
+    };
+
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post<SuggestionAddressResponseDto>(requestUrl, body, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .pipe(
+          catchError((error: AxiosError) => {
+            this.logger.error(
+              `Get address failed: ${JSON.stringify(error.response?.data)}`,
+              error.stack,
+              context,
+            );
+            throw new GoogleMapException(
+              GoogleMapValidation.ERROR_WHEN_GET_ADDRESS,
+              error.message,
+            );
+          }),
+        ),
+    );
+
+    return data.suggestions;
+  }
+
   async getLocationByPlaceId(placeId: string): Promise<LocationResponseDto> {
     const context = `${GoogleMapConnectorClient.name}.${this.getLocationByPlaceId.name}`;
-    const requestUrl = `${await this.googleMapsApiUrl}/place/details/json?place_id=${placeId}&key=${this.googleMapsApiKey}`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/place/details/json?place_id=${placeId}&key=${this.googleMapsApiKey}`;
 
     const { data } = await firstValueFrom(
       this.httpService
@@ -129,7 +230,7 @@ export class GoogleMapConnectorClient {
     placeId: string,
   ): Promise<PlaceDetailsResultResponseDto> {
     const context = `${GoogleMapConnectorClient.name}.${this.getLocationByPlaceId.name}`;
-    const requestUrl = `${await this.googleMapsApiUrl}/place/details/json?place_id=${placeId}&key=${this.googleMapsApiKey}`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/place/details/json?place_id=${placeId}&key=${this.googleMapsApiKey}`;
 
     const { data } = await firstValueFrom(
       this.httpService
@@ -172,7 +273,7 @@ export class GoogleMapConnectorClient {
     destination: string,
   ): Promise<RouteAndDirectionResponseDto> {
     const context = `${GoogleMapConnectorClient.name}.${this.getDirection.name}`;
-    const requestUrl = `${await this.googleMapsApiUrl}/directions/json`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/directions/json`;
 
     const { data } = await firstValueFrom(
       this.httpService
@@ -222,7 +323,7 @@ export class GoogleMapConnectorClient {
     destinations: string,
   ): Promise<DistanceAndDurationResponseDto> {
     const context = `${GoogleMapConnectorClient.name}.${this.getDistanceAndDuration.name}`;
-    const requestUrl = `${await this.googleMapsApiUrl}/distancematrix/json`;
+    const requestUrl = `${await this.getGoogleMapsApiUrl()}/distancematrix/json`;
 
     const { data } = await firstValueFrom(
       this.httpService
