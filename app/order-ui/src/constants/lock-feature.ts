6769e76a513b9f7d206ffca794e10e65e8b849a9
QuyetThangDev <quyetthang0501@gmail.com>
2025-10-04 10:50:51 +0700

    TaskId: TRE-247-FE (1) Implement feature lock management
    Type: Development
    
    Description:
    - Implement feature lock management

---
+++ app/order-ui/src/constants/lock-feature.ts
@@ -0,0 +1,10 @@
+export enum SystemLockFeatureGroup {
+  GROUP_EXAMPLE = 'GROUP_EXAMPLE',
+}
+
+export enum SystemLockFeatureType {
+  SELF = 'SELF',
+  GIFT = 'GIFT',
+  BUY = 'BUY',
+  NONE = 'NONE',
+}
