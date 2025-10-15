182ea40f13364afeba352dcd5bf9a35c1d25b410
huu thiet <huuthietn01@gmail.com>
2025-10-15 10:53:14 +0700

    * Parent ID: TRE-270-BE (1) Task Id: TRE-270-BE Type: Development Description: - Forgot password send code to mail, sms

--- app/order-api/src/auth/auth.controller.ts
+++ app/order-api/src/auth/auth.controller.ts
@@ -28,6 +28,8 @@ import {
   VerifyEmailResponseDto,
   VerifyPhoneNumberResponseDto,
   ConfirmPhoneNumberVerificationCodeRequestDto,
+  ForgotPasswordResponseDto,
+  ConfirmForgotPasswordRequestDto,
 } from './auth.dto';
 import {
   ApiBearerAuth,
@@ -360,11 +362,11 @@ export class AuthController {
 
   @Public()
   @HttpCode(HttpStatus.OK)
-  @Post('forgot-password/token')
+  @Post('forgot-password/initiate')
   @ApiOperation({ summary: 'Create forgot password token' })
   @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
   @ApiResponseWithType({
-    type: AuthProfileResponseDto,
+    type: ForgotPasswordResponseDto,
     description: 'Token created successfully',
   })
   async createForgotPasswordToken(
@@ -378,6 +380,51 @@ export class AuthController {
       statusCode: HttpStatus.OK,
       timestamp: new Date().toISOString(),
       result,
+    } as AppResponseDto<ForgotPasswordResponseDto>;
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('forgot-password/resend')
+  @ApiOperation({ summary: 'Resend forgot password token' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
+  @ApiResponseWithType({
+    type: ForgotPasswordResponseDto,
+    description: 'Token resend successfully',
+  })
+  async resendForgotPasswordToken(
+    @Body(new ValidationPipe({ transform: true }))
+    requestData: ForgotPasswordTokenRequestDto,
+  ) {
+    const result =
+      await this.authService.resendForgotPasswordToken(requestData);
+    return {
+      message: 'Token resend successfully',
+      statusCode: HttpStatus.OK,
+      timestamp: new Date().toISOString(),
+      result,
+    } as AppResponseDto<ForgotPasswordResponseDto>;
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('forgot-password/confirm')
+  @ApiOperation({ summary: 'Confirm forgot password token' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
+  @ApiResponseWithType({
+    type: String,
+    description: 'Token confirmed successfully',
+  })
+  async confirmForgotPassword(
+    @Body(new ValidationPipe({ transform: true }))
+    requestData: ConfirmForgotPasswordRequestDto,
+  ) {
+    await this.authService.confirmForgotPassword(requestData);
+    return {
+      message: 'Token confirmed successfully',
+      statusCode: HttpStatus.OK,
+      timestamp: new Date().toISOString(),
+      result: 'Forgot password confirmed successfully',
     } as AppResponseDto<string>;
   }
 
--- app/order-api/src/auth/auth.validation.ts
+++ app/order-api/src/auth/auth.validation.ts
@@ -44,6 +44,8 @@ export const VERIFY_PHONE_NUMBER_TOKEN_IS_EXPIRED =
 export const CONFIRM_PHONE_NUMBER_VERIFICATION_ERROR =
   'CONFIRM_PHONE_NUMBER_VERIFICATION_ERROR';
 export const USER_NOT_ACTIVE = 'USER_NOT_ACTIVE';
+export const USER_NOT_VERIFIED_EMAIL = 'USER_NOT_VERIFIED_EMAIL';
+export const USER_NOT_VERIFIED_PHONENUMBER = 'USER_NOT_VERIFIED_PHONENUMBER';
 
 export type TAuthErrorCodeKey =
   | typeof INVALID_PHONENUMBER
@@ -79,7 +81,9 @@ export type TAuthErrorCodeKey =
   | typeof VERIFY_PHONE_NUMBER_TOKEN_NOT_FOUND
   | typeof VERIFY_PHONE_NUMBER_TOKEN_IS_EXPIRED
   | typeof CONFIRM_PHONE_NUMBER_VERIFICATION_ERROR
-  | typeof USER_NOT_ACTIVE;
+  | typeof USER_NOT_ACTIVE
+  | typeof USER_NOT_VERIFIED_EMAIL
+  | typeof USER_NOT_VERIFIED_PHONENUMBER;
 
 export type TAuthErrorCode = Record<TAuthErrorCodeKey, TErrorCodeValue>;
 
@@ -164,4 +168,9 @@ export const AuthValidation: TAuthErrorCode = {
     'Error when confirm phone number verification',
   ),
   USER_NOT_ACTIVE: createErrorCode(119033, 'User not active'),
+  USER_NOT_VERIFIED_EMAIL: createErrorCode(119034, 'User not verified email'),
+  USER_NOT_VERIFIED_PHONENUMBER: createErrorCode(
+    119035,
+    'User not verified phone number',
+  ),
 };
