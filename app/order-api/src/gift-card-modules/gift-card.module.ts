9d37b8dbb4e09a5b7164f32977415b9bbc42fecc
tuan.do <doduongthaituan201102@gmail.com>
2025-10-20 01:50:51 +0700

    * Parent ID: GC-150-BE (1) Task Id: GC-171 Type: Development Description: + Implement API

--- app/order-api/src/gift-card-modules/gift-card.module.ts
+++ app/order-api/src/gift-card-modules/gift-card.module.ts
@@ -46,6 +46,10 @@ import { FeatureGroup } from './feature-flag/entities/feature-group.entity';
 import { SharedModule } from 'src/shared/shared.module';
 import { CashStrategy } from 'src/payment/strategy/cash.strategy';
 import { PaymentUtils } from 'src/payment/payment.utils';
+import { CoinPolicy } from './coin-policy/entities/coin-policy.entity';
+import { CoinPolicyController } from './coin-policy/coin-policy.controller';
+import { CoinPolicyService } from './coin-policy/coin-policy.service';
+import { CoinPolicyProfile } from './coin-policy/coin-policy.mapper';
 
 const controllers = [
   CardController,
@@ -55,9 +59,11 @@ const controllers = [
   ReceipientController,
   PointTransactionController,
   FeatureFlagController,
+  CoinPolicyController
 ];
 
 const providers = [
+  CoinPolicyService,
   CardService,
   BalanceService,
   CardOrderService,
@@ -80,6 +86,7 @@ const mappers = [
   GiftCardProfile,
   PointTransactionProfile,
   FeatureFlagProfile,
+  CoinPolicyProfile
 ];
 
 const modules = [
@@ -96,6 +103,7 @@ const modules = [
     Order,
     FeatureFlag,
     FeatureGroup,
+    CoinPolicy
   ]),
   FileModule,
   DbModule,
@@ -113,6 +121,7 @@ const exportServices = [
   RecipientService,
   PointTransactionService,
   FeatureFlagService,
+  CoinPolicyService
 ];
 
 const listeners = [CardOrderListener];
