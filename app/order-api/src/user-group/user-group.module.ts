95eb4ab04b1b8b76fd6e7daf24749a7e39da0e4a
huu thiet <huuthietn01@gmail.com>
2025-10-21 08:53:23 +0700

    * Parent ID: TRE-276-BE (1) Task Id: TRE-276-BE Type: Development Description: - Delete member when delete group

--- app/order-api/src/user-group/user-group.module.ts
+++ app/order-api/src/user-group/user-group.module.ts
@@ -5,11 +5,12 @@ import { UserGroupController } from './user-group.controller';
 import { UserGroup } from './user-group.entity';
 import { UserGroupProfile } from './user-group.mapper';
 import { User } from 'src/user/user.entity';
+import { TransactionManagerService } from 'src/db/transaction-manager.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserGroup, User])],
   controllers: [UserGroupController],
-  providers: [UserGroupService, UserGroupProfile],
+  providers: [UserGroupService, UserGroupProfile, TransactionManagerService],
   exports: [UserGroupService],
 })
 export class UserGroupModule {}
