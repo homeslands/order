b1113bc1edd33380b0a89d2414119b094cecfcb9
QuyetThangDev <quyetthang0501@gmail.com>
2025-10-21 08:30:15 +0700

    TaskId: TRE-274-FE (5) Setup environment for building mobile app
    Type: Development
    
    Description:
    - Update app icon

--- app/order-ui/src/app/layouts/client/ClientLayout.tsx
+++ app/order-ui/src/app/layouts/client/ClientLayout.tsx
@@ -48,7 +48,7 @@ export default function ClientLayout() {
 
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen safe-area">
       {/* Header */}
       <ClientHeader />
 
--- app/order-ui/src/app/layouts/client/client-layout-public.tsx
+++ app/order-ui/src/app/layouts/client/client-layout-public.tsx
@@ -46,7 +46,7 @@ export default function PublicClientLayout() {
   }, [location.pathname, clearStore, userInfo, navigate])
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen safe-area">
       {/* Header */}
       <ClientHeader />
 
