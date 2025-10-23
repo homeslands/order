62caf79e08e40200745eb45294490520267448d2
tuan.do <doduongthaituan201102@gmail.com>
2025-10-23 22:03:06 +0700

    * Parent ID: GC-150-FE (5) Task Id: GC-263,264,265 Type: Development Description: + Fix UI

--- app/order-ui/src/app/system/coin-policy/components/coin-policy-item.tsx
+++ app/order-ui/src/app/system/coin-policy/components/coin-policy-item.tsx
@@ -2,12 +2,14 @@ import { ICoinPolicy } from "@/types/coin-policy.type"
 import { formatCurrency } from "@/utils"
 import CoinPolicyItemActions from "./coin-policy-item-actions"
 import { CoinPolicyConstants } from "@/constants/coin-policy"
+import { useTranslation } from "react-i18next"
 
 export interface CoinPolicyItemProps {
   data: ICoinPolicy
 }
 
 export default function CoinPolicyItem({ data }: CoinPolicyItemProps) {
+  const { t } = useTranslation(['coinPolicy'])
   const renderValue = (payload: { key: string, value: string }) => {
     switch (payload.key) {
       case CoinPolicyConstants.MAX_BALANCE:
@@ -16,17 +18,27 @@ export default function CoinPolicyItem({ data }: CoinPolicyItemProps) {
         return payload.value
     }
   }
+
+  const renderLabel = (payload: { key: string, value: string }) => {
+    switch (payload.key) {
+      case CoinPolicyConstants.MAX_BALANCE:
+        return t('coinPolicy.maxBalance')
+      default:
+        return payload.value
+    }
+  }
+
   return (
     <>
       <div className="mt-2 flex flex-col gap-2 p-2 rounded-md dark:bg-transparent lg:flex-row lg:items-center">
         {/* Left */}
         <div className="w-2/5 text-sm font-bold">
-          {data.name}:
+          {renderLabel({ key: data.key, value: data.name })}:
         </div>
 
         {/* Center */}
         <div className="w-full flex items-center gap-4">
-          <div className="border p-2 text-sm rounded-sm bg-gray-100 w-full"
+          <div className="border p-2 text-sm rounded-sm bg-gray-100 w-full dark:bg-transparent dark:border-gray-500"
           >
             {renderValue({ key: data.key, value: data.value })}
           </div>
